1fff2a45b75908f00d60412b066c4b78ebeef50a
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-20 06:41:25 -0500

    Initial Commit

---
+++ WhiteHousePetitionsHWSProgrammatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhiteHousePetitionsHWSProgrammatic
+//
+//  Created by Herve Desrosiers on 2020-02-20.
+//  Copyright © 2020 Herve Desrosiers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
